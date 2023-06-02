c35e73cafec1462fd94894ec5840430d85a42561
MrGauws <engstromhlol@gmail.com>
2023-06-02 13:36:09 +0200

    Error message, nätverks message + comments

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
-const apiKey = "087dd72990ff1dacfef4966e28455ebd"; // The API key used to access the OpenWeatherMap API.
-const form = document.getElementById("cityForm"); // The form-element with the id "cityForm".
-const cityInput = document.getElementById("cityInput"); //The input-element with the id "cityInput" which is used to input the city name.
-const weatherInfo = document.getElementById("weatherInfo"); // The element with the id "weatherInfo" which is used to display the weather information.
-const forecastContainer = document.getElementById("forecastContainer"); // The element with the id "forecastContainer" which is used to display the weather forecast.
-const forecastHoursSelect = document.getElementById("forecastHours"); // The element with the id "forecastHours" which is used to select the number of forecast hours.
+// ------------ Variable declarations ------------ //
 
-// Event listener for form submission
+// The API key used to access the OpenWeatherMap API. 
+const apiKey = "087dd72990ff1dacfef4966e28455ebd"; 
+
+// DOM elements
+const form = document.getElementById("cityForm"); 
+const cityInput = document.getElementById("cityInput"); 
+const weatherInfo = document.getElementById("weatherInfo"); 
+const forecastContainer = document.getElementById("forecastContainer"); 
+const forecastHoursSelect = document.getElementById("forecastHours"); 
+
+// ------------ Event listener for form submission ------------ //
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const city = cityInput.value;
-  const forecastHours = forecastHoursSelect.value; // Collects the selected value from the forecast hours select element.
+  // Collects the selected value from the forecast hours select element.
+  const forecastHours = forecastHoursSelect.value; 
   getWeather(city);
-  getForecast(city, forecastHours); // Collects the entered city and selected forecast hours and fetches and displays the weather forecast.
-  cityInput.value = ""; // Clears the value of the city input field after submitting the form.
+  // Collects the entered city and selected forecast hours and fetches and displays the weather forecast.
+  getForecast(city, forecastHours); 
+  // Clears the value of the city input field after submitting the form.
+  cityInput.value = ""; 
 });
 
-// Retrieve coordinates for a given city.
+// Retrieve coordinates for a given city. Using async to handle asynchronous operations since im using await.
 async function getCoordinatesByCity(city) {
   try {
     // Request OpenWeatherMap API to fetch the coordinates of the specified city.
@@ -39,13 +47,15 @@ async function getWeather(city) {
   try {
     let coordinates = {};
     if (city) {
+      // If a city name is provided, asynchronously fetch its coordinates. Using await to pause execution until coordinates are fetched
       coordinates = await getCoordinatesByCity(city);
     } else {
+      // If no city name is provided, throw an error indicating it is mandatory
       throw new Error('Stadens namn är obligatoriskt.');
     }
 
     const { latitude, longitude } = coordinates;
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric&lang=sv`);
     const data = await response.json();
     displayWeather(data);
   } catch (error) {
@@ -53,10 +63,10 @@ async function getWeather(city) {
   }
 }
 
-// Retrieve weather forecast data for a city. Async enables handling asynchronous operations with 'await fetch', improving code readability.
+// Retrieve weather forecast data for a city
 async function getForecast(city, forecastHours) {
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&lang=sv`);
     if (response.ok) {
       const data = await response.json();
       displayForecast(data, forecastHours);
@@ -127,16 +137,19 @@ function displayForecast(data, forecastHours) {
 
   // Iterates through forecast intervals
   for (let i = 0; i < numIntervals; i++) {
+    // Gathers the forecast data for the current interval
     const forecast = forecastList[i];
+    // Convert the forecast time to a Date object
     const forecastTime = new Date(forecast.dt_txt);
     const day = getDayOfWeek(forecastTime);
+    // Formats the forecast time as a string in the desired format
     const forecastTimeFormatted = forecastTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
     const forecastTemperature = forecast.main.temp;
     const forecastWeatherCode = forecast.weather[0].id;
     const forecastDescription = forecast.weather[0].description;
     const forecastWindSpeed = forecast.wind.speed;
 
-    // Determine weather class for forecast
+    // Determine weather class for forecast. I've used an if else statement here since there was no other way to get the boxes to change colour accordingly.
     let weatherClass = "";
     if (forecastWeatherCode >= 200 && forecastWeatherCode < 600) {
       weatherClass = "rainy";
@@ -164,15 +177,17 @@ function displayForecast(data, forecastHours) {
   forecastContainer.innerHTML = forecastHTML;
 }
 
-// Display error message
-function displayError(message) {
-  weatherInfo.innerHTML = `<p class="error">${message}</p>`;
-  forecastContainer.innerHTML = "";
+// Get the day of the week for a given date. Created an array to show what day it is.
+function getDayOfWeek(date) {
+  const days = ["Sön", "Mån", "Tis", "Ons", "Tors", "Fre", "Lör"];
+  return days[date.getDay()];
 }
 
-// Get the day of the week
-function getDayOfWeek(date) {
-  const days = ["Sön", "Mån", "Tis", "Ons", "Tor", "Fre", "Lör"];
-  const dayIndex = date.getDay();
-  return days[dayIndex];
+// Displays the error message and checks for network.
+function displayError(errorMessage) {
+  if (navigator.onLine) {
+    alert(errorMessage);
+  } else {
+    alert("Det uppstod ett nätverksfel. Kontrollera din anslutning och försök igen.");
+  }
 }
